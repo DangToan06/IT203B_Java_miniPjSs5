3da0f33692a5780029cdf56c78a66ac75fd8f3a0
latp1102 <Latp1102@gmail.com>
2026-03-12 11:18:29 +0700

    Update OrderServiceTest.java

--- src/test/java/com/fastfood/OrderServiceTest.java
+++ src/test/java/com/fastfood/OrderServiceTest.java
@@ -9,22 +9,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class OrderServiceTest {
     @Test
     void testCalculateTotal() {
-        // tạo menu item
         MenuItem item = new MenuItem("F1", "Burger", 50000, 10) {
             @Override
             public double calculatePrice() {
                 return getPrice();
             }
         };
-        // tạo order item
         OrderItem orderItem = new OrderItem(item, 2);
-        // tạo order
         Order order = new Order();
-        // thêm item vào order
         order.addItem(orderItem);
-        // tính tổng
         double total = order.calculateTotal();
-        // kiểm tra kết quả
         assertEquals(100000, total);
     }
 }
