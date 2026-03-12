2ac95f114cc6871d87a53771a6a315c8b5737f0d
latp1102 <Latp1102@gmail.com>
2026-03-12 11:16:22 +0700

    Update OrderServiceTest.java

--- src/test/java/com/fastfood/OrderServiceTest.java
+++ src/test/java/com/fastfood/OrderServiceTest.java
@@ -3,36 +3,27 @@ package com.fastfood.service;
 import com.fastfood.model.MenuItem;
 import com.fastfood.model.Order;
 import com.fastfood.model.OrderItem;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceTest {
-
     @Test
     void testCalculateTotal() {
-
-        // tạo menu item thật
+        // tạo menu item
         MenuItem item = new MenuItem("F1", "Burger", 50000, 10) {
             @Override
             public double calculatePrice() {
                 return getPrice();
             }
         };
-
         // tạo order item
         OrderItem orderItem = new OrderItem(item, 2);
-
         // tạo order
         Order order = new Order();
-
         // thêm item vào order
         order.addItem(orderItem);
-
         // tính tổng
         double total = order.calculateTotal();
-
         // kiểm tra kết quả
         assertEquals(100000, total);
     }
